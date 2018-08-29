2275cec3fb69998bd35e236d7a01ce6ba8c35d37
tntll <tntll@hotmail.com>
2018-08-29 13:01:22 +0800

    空链表考虑后修改

--- AlgorithmsByPython/Target Offer/反向打印链表.py
+++ AlgorithmsByPython/Target Offer/反向打印链表.py
@@ -1,14 +1,17 @@
 '''
 输入一个链表，从尾到头打印链表每个节点的值。
 '''
+
+
 class ListNode:
     def __init__(self, x=None):
         self.val = x
         self.next = None
 
+
 class Solution:
     def printListFromTailToHead(self, listNode):
-        if listNode.val == None:
+        if listNode == None:
             return
         l = []
         head = listNode
@@ -16,6 +19,7 @@ class Solution:
             l.insert(0, head.val)
             head = head.next
         return l
+
 
 node1 = ListNode(10)
 node2 = ListNode(11)
